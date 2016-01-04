a67d77d1eb7a4e16e9dea294437a96ab653fec43
starkwang <381152119@qq.com>
2016-01-04 23:12:47 +0800

    评论框

--- client/src/components/comment/commentController.js
+++ client/src/components/comment/commentController.js
@@ -1,3 +1,40 @@
-module.exports = ['$scope', '$rootScope', function($scope, $rootScope) {
-    
+module.exports = ['$scope', '$rootScope', 'BaseService', function($scope, $rootScope, BaseService) {
+    function init() {
+        var itemID = window.location.pathname.split('/')[2];
+        // BaseService.comment.get(itemID).then(function(result) {
+
+        // });
+        $scope.comments = [{
+            underWhichItem: '12312412124',
+            ownerID: '12345667',
+
+            publisherName: 'starkwang',
+            publisherID: '123123412413',
+
+            isReply: false,
+            targetName: '',
+            targetID: '',
+
+            content: '这是评论这是评论这是评论这是评论这是评论这是评论',
+            time: '2015-10-20 12:43:43',
+
+            haveBeenRead: false
+        },{
+            underWhichItem: '12312412124',
+            ownerID: '12345667',
+
+            publisherName: 'starkwang',
+            publisherID: '123123412413',
+
+            isReply: true,
+            targetName: 'tony',
+            targetID: '',
+
+            content: '这是回复这是回复这是回复这是回复这是回复这是回复',
+            time: '2015-10-20 12:43:43',
+
+            haveBeenRead: false
+        }];
+    }
+    init();
 }]
