5628bf19361ea6f0792293f485883498bdbd16dd
starkwang <381152119@qq.com>
2016-01-14 21:07:20 +0900

    upload

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,4 +1,7 @@
 var gm = require('gm');
+var service = require('../service/service');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require("fs"));
 
 function upload(req, res, next) {
     // req.files is array of `photos` files
@@ -8,16 +11,26 @@ function upload(req, res, next) {
     try {
         var fileName = req.file.path.split('/')[1].split('.')[0];
         var fileType = req.file.path.split('/')[1].split('.')[1];
-        gm(req.file.path).resize(600, 600).write('img/' + fileName + '-small.' + fileType, function(err) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("minify image done");
-            }
-        });
+
+        fs.readFileAsync('img/' + fileName + '.' + fileType)
+            .then(function(file) {
+                return fs.writeFileAsync('img/small/' + fileName + '.' + fileType)
+            })
+            .then(function() {
+                gm(req.file.path).resize(400).write('img/small/' + fileName + '.' + fileType, function(err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("minify image done");
+                        //service.item.
+                    }
+                });
+            });
+
     } catch (e) {
         console.log(e);
     }
+
 
 
     res.send({
