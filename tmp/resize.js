80d31b8765128e72762ef1eac73e79f065544c35
starkwang <381152119@qq.com>
2016-01-21 11:32:41 +0800

    压缩图片

---
+++ tmp/resize.js
@@ -0,0 +1,16 @@
+var Promise = require("bluebird");
+var join = Promise.join;
+var fs = Promise.promisifyAll(require("fs"));
+var gm = require('gm');
+fs.readdirAsync("../img").map(function(fileName) {
+    gm('../img/' + fileName)
+        .resize(400)
+        .write('../img/small/' + fileName, function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("minify image done");
+                //service.item.
+            }
+        });
+})
